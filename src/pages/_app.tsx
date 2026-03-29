c658a2070b4b4a44696d79e20900692d8c7400b5
boilerrat <128boilerrat@gmail.com>
2026-03-29 17:49:14 -0400

    remove google

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,15 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
-import { Fraunces, Inter } from "next/font/google";
 import "../styles/globals.css";
 
 import { ThemeProvider } from "../components/ThemeProvider";
 import { ErrorBoundary } from "../components/ErrorBoundary";
 
-const fraunces = Fraunces({
-  subsets: ["latin"],
-  variable: "--font-fraunces",
-  display: "swap",
-});
-
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-  display: "swap",
-});
-
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <ThemeProvider>
-        <div className={`${fraunces.variable} ${inter.variable}`}>
+        <div>
           <ErrorBoundary>
             <Component {...pageProps} />
           </ErrorBoundary>
